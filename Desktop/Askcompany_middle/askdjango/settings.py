00a794869c396c9b68844ec56bb17286b986115f
rita5811 <rita5811@snu.ac.kr>
2020-07-27 16:51:38 +0900

    4강 로그아웃 구현하기

--- Desktop/Askcompany_middle/askdjango/settings.py
+++ Desktop/Askcompany_middle/askdjango/settings.py
@@ -126,5 +126,6 @@ STATIC_URL = '/static/'
 
 from django.urls import reverse_lazy
 
-LOGIN_URL = reverse_lazy('login')
-LOGIN_REDIRECT_URL = reverse_lazy('profile')
+#LOGIN_URL = reverse_lazy('login')
+#LOGIN_REDIRECT_URL = reverse_lazy('profile')
+#LOGOUT_REDIRECT_URL = reverse_lazy('login')
