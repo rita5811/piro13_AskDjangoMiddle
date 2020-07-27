42b89b67117f4f6b65f805df3a2158c3dd001838
rita5811 <rita5811@snu.ac.kr>
2020-07-27 18:25:09 +0900

    8강 회원가입 시에 next인자 처리하기

--- Desktop/Askcompany_middle/accounts/views.py
+++ Desktop/Askcompany_middle/accounts/views.py
@@ -19,7 +19,8 @@ def signup(request):
         if form.is_valid():
             user = form.save() #이 순간이 user가 생성되는 것
             auth_login(request, user)
-            return redirect('profile')
+            next_url = request.GET.get('next') or 'profile'
+            return redirect(next_url)
     else:
         form = SignupForm()
     return render(request, 'accounts/signup.html',{
@@ -33,7 +34,8 @@ class SignupView(CreateView): #클래스 기반 뷰
     template_name = 'accounts/signup.html'
 
     def get_success_url(self):
-        return resolve_url('profile')
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
 
     def form_valid(self, form):
         user = form.save()
