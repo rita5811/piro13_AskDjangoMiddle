0ccb47e859938c8e04b1f40252de0e0ad65dd937
rita5811 <rita5811@snu.ac.kr>
2020-07-27 15:46:47 +0900

    2강 로그인 구현하기

--- Desktop/Askcompany_middle/askdjango/urls.py
+++ Desktop/Askcompany_middle/askdjango/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
-    path('', lambda req: redirect('blog/')), #URL reverse
+    path('', lambda req: redirect('blog:post_list')), #URL reverse
 ]
--- Desktop/Askcompany_middle/blog/urls.py
+++ Desktop/Askcompany_middle/blog/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:pk>/', views.post_detail, name='post_detail'),
