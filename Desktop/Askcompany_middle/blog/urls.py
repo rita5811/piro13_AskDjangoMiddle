78ba6c9c360a9f1b0596b9c35ba7a26c01b43b36
rita5811 <rita5811@snu.ac.kr>
2020-07-27 14:52:08 +0900

    1강 장고 프로젝트 생성 및 구성

---
+++ Desktop/Askcompany_middle/blog/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+]
